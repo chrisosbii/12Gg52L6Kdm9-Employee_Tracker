29bdd63d1577fe35d1b3aeb461af59b98dd474bb
Chris Osborne <chrisosbii@gmail.com>
2023-08-10 00:27:01 -0500

    updated options and prety printing

--- db/schema.sql
+++ db/schema.sql
@@ -12,6 +12,7 @@ CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
+  department_id INT,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
@@ -25,6 +26,7 @@ CREATE TABLE employee (
   FOREIGN KEY (role_id)
   REFERENCES role(id)
   ON DELETE SET NULL,
+  manager_id INT,
   FOREIGN KEY (manager_id)
   REFERENCES employee(id)
   ON DELETE SET NULL
--- index.js
+++ index.js
@@ -16,6 +16,8 @@ const db = mysql.createConnection(
     console.log(`Connected to the eployees_db database.`)
 );
 
+// print intro
+printIntro();
 // load main menu
 mainMenu();
 
@@ -43,16 +45,20 @@ function mainMenu(){
                 viewEmployees();
                 mainMenu();
                 break;
-            case 'add a departments':
+            case 'add a department':
+                addDepartment();
                 mainMenu();
                 break;
-            case 'add a roles':
+            case 'add a role':
+                addRole();
                 mainMenu();
                 break;
-            case 'add a employees':
+            case 'add an employee':
+                addEmployee();
                 mainMenu();
                 break;
             case 'update an employee role':
+                updateRole();
                 mainMenu();
                 break;
             case 'exit':
@@ -74,8 +80,8 @@ function viewDepartments(){
             res.status(500).json({ error: err.message });
             return;
         }
-        console.log(results);
-        console.log(fields);
+        prettyDepartments(results);
+        //console.log(fields);
     });
     return true;
 }
@@ -89,7 +95,7 @@ function viewRoles(){
             return;
         }
         console.log(results);
-        console.log(fields);
+        //console.log(fields);
     });
     return true;
 }
@@ -103,7 +109,135 @@ function viewEmployees(){
             return;
         }
         console.log(results);
-        console.log(fields);
+        //console.log(fields);
     });
+    return true;
+}
+function addDepartment(){
+    console.log("in add department");
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Please input name of new department:',
+            name: 'name'
+        }
+    ]).then((data) => {
+        const sql = `INSERT INTO department (name)
+         VALUES (?)`;
+        const params = [data.name];
+
+        db.query(sql, params, (err, results, fields) => {
+            if (err) {
+                res.status(400).json({ error: err.message });
+                return;
+            }
+            res.json({
+                message: 'success',
+                data: body
+            });
+        });
+    });
+    
+    return true;
+}
+function addRole(){
+    console.log("in add department");
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Please input name of new role:',
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: 'Please input salary of new role:',
+            name: 'salary'
+        },
+        {
+            type: 'input',
+            message: 'Please input department:',
+            name: 'department'
+        },
+
+    ]).then((data) => {
+        const sql = `INSERT INTO role (name)
+         VALUES (?)`;
+        const params = [data.name];
+
+        db.query(sql, params, (err, results, fields) => {
+            if (err) {
+                res.status(400).json({ error: err.message });
+                return;
+            }
+            res.json({
+                message: 'success',
+                data: body
+            });
+        });
+    });
+    
+    return true;
+}
+function addEmployees(){
+    console.log("in add employee");
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Please input first name of new employee:',
+            name: 'first'
+        },
+        {
+            type: 'input',
+            message: 'Please input last name of new employee:',
+            name: 'last'
+        },
+        {
+            type: 'input',
+            message: 'Please input role of new employee:',
+            name: 'role'
+        },
+        {
+            type: 'input',
+            message: 'Please input manager of new employee:',
+            name: 'manager'
+        }
+    ]).then((data) => {
+        const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+         VALUES (?)`;
+        const params = [data];
+
+        db.query(sql, params, (err, results, fields) => {
+            if (err) {
+                res.status(400).json({ error: err.message });
+                return;
+            }
+            res.json({
+                message: 'success',
+                data: body
+            });
+        });
+    });
+    return true;
+}
+
+function printIntro() {
+    console.log(".___________________.");
+    console.log("|                   |");
+    console.log("| EMPLOYEE DATABASE |");
+    console.log("|___________________|");
+    console.log("");
+}
+
+function prettyDepartments(words){
+    var temp = JSON.parse(words);
+    console.log("id  department name");
+    console.log("--  ---------------");
+    for(var i = 0; i < temp.length; i++){
+        console.log(temp[i].id + "   " + temp[i].name);
+    }
+    console.log()
     return true;
 }
